f51ca7bc4c3276a9ca6e95a5cf9a0817fef36a78
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-07-22 21:53:30 +0300

    function

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,7 +1,11 @@
-import clock from "../_data/clock.ts";
+import clock from "./clock.ts";
+
+function updateClock(): void {
+  clock.tick();
+
+  globalThis.setInterval(updateClock, 1000);
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-  clock.updateClock();
-
-  globalThis.setInterval(clock.updateClock, 1000);
+  updateClock();
 });

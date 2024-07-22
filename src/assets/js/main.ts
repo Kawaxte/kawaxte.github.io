8f8aa090fb241d9c04d3db348c1def9fcb13f379
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-07-22 07:15:50 +0300

    serve by oak

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -1,1 +1,23 @@
-console.log("Hello, world!");
+import { Application, Router } from "@oak/oak";
+
+const router = new Router();
+
+const app = new Application();
+app.use(async (ctx, next) => {
+  try {
+    await ctx.send({
+      root: `${Deno.cwd()}/_site`,
+      index: "index.html",
+    });
+  } catch {
+    next();
+  }
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+async function listen(): Promise<void> {
+  await app.listen({ port: 8000 });
+}
+
+listen();

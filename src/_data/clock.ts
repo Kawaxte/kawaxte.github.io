79d8902a1954d61fcc32f6d9f446682c69751c2c
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-07-22 14:08:59 +0300

    initial commit

---
+++ src/_data/clock.ts
@@ -0,0 +1,22 @@
+export default {
+  updateClock: (): void => {
+    const clock = document.getElementById("clock");
+    if (clock) {
+      const now = new Date();
+
+      const options: Intl.DateTimeFormatOptions = {
+        timeZone: "Europe/Tallinn",
+        day: "2-digit",
+        month: "short",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      };
+
+      clock.textContent = now.toLocaleString("en-GB", options).replace(
+        ",",
+        "",
+      );
+    }
+  },
+};
---
+++ src/_data/data.ts
@@ -0,0 +1,7 @@
+export default {
+  getCopyright: () => {
+    const now = new Date();
+
+    return `${now.getFullYear()} Krystian`;
+  },
+};

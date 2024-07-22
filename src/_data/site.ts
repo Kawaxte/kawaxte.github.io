445239f3312627effc7f32cf8fac53ab69dd224c
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-07-22 21:52:40 +0300

    initial commit

---
+++ src/_data/site.ts
@@ -0,0 +1,6 @@
+export default {
+  github: "https://github.com/Kawaxte",
+  steam: "https://steamcommunity.com/id/Kawaxte",
+  youtube: "https://www.youtube.com/@krysyxte303",
+  copyright: `${new Date().getFullYear()} Krystian`,
+};

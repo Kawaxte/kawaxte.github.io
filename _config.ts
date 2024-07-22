3dcccf8927609a22476dfab7720ae47a03810fbb
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2024-07-22 06:41:26 +0300

    use Vento over Nunjucks

--- _config.ts
+++ _config.ts
@@ -1,14 +1,40 @@
+import esbuild from "lume/plugins/esbuild.ts";
 import lume from "lume/mod.ts";
 import sass from "lume/plugins/sass.ts";
-import postcss from "lume/plugins/postcss.ts";
-import terser from "lume/plugins/terser.ts";
+import vento from "lume/plugins/vento.ts";
 
-const site = lume();
+const site = lume({
+  src: "./src",
+  dest: "./_site",
+  includes: "./src/_includes",
+});
 
-site.use(sass());
-site.use(postcss());
-site.use(terser());
-
-site.copy("static");
-
-export default site;
+export default site
+  .ignore("README.md")
+  .use(esbuild({
+    extensions: [".ts"],
+    options: {
+      platform: "browser",
+      entryPoints: ["assets/ts/main.ts"],
+      format: "esm",
+      splitting: true,
+      minify: true,
+      treeShaking: true,
+    },
+  }))
+  .use(vento({
+    extensions: [".vto"],
+    options: {
+      dataVarname: "data",
+      autoescape: true,
+    },
+  }))
+  .use(sass({
+    extensions: [".scss", ".sass"],
+    options: {
+      style: "compressed",
+      alertAscii: true,
+      alertColor: true,
+    },
+  }))
+  .copy("assets/media");

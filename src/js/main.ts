0b11b25d0285b0a5b0c695322d46bdd620ecc357
krysyxte <31629699+Kawaxte@users.noreply.github.com>
2024-07-25 19:38:45 +0300

    separations

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,31 +1,7 @@
+import { initContactForm } from "./contact_form.ts";
+import { initThemeToggle } from "./theme_toggle.ts";
+
 document.addEventListener("DOMContentLoaded", () => {
-  const themeToggle = document.querySelector(
-    ".sl-c-header__theme-toggle",
-  ) as HTMLElement;
-
-  const sunIcon = themeToggle.querySelector(".fa-sun") as HTMLElement;
-  const moonIcon = themeToggle.querySelector(".fa-moon") as HTMLElement;
-
-  function toggleTheme(): void {
-    const isDark = document.documentElement.style.colorScheme === "dark";
-    document.documentElement.style.colorScheme = isDark ? "light" : "dark";
-
-    sunIcon.style.display = isDark ? "inline-block" : "none";
-    moonIcon.style.display = isDark ? "none" : "inline-block";
-
-    localStorage.setItem("theme", isDark ? "light" : "dark");
-  }
-
-  const prefersDark = globalThis.matchMedia("(prefers-color-scheme: dark)");
-
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme === "dark" || (!savedTheme && prefersDark.matches)) {
-    toggleTheme();
-  }
-
-  themeToggle.addEventListener("click", toggleTheme);
-
-  prefersDark.addEventListener("change", () => {
-    if (!localStorage.getItem("theme")) toggleTheme();
-  });
+  initThemeToggle();
+  initContactForm();
 });

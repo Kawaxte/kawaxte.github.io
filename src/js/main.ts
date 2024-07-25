a9e301e0c862110342b02f4250d330d9b36bf951
krysyxte <31629699+Kawaxte@users.noreply.github.com>
2024-07-26 00:44:45 +0300

    tablet/mobile friendliness

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,7 +1,10 @@
 import { initContactForm } from "./contact_form.ts";
+import { initMobileMenu } from "./mobile_menu.ts";
 import { initThemeToggle } from "./theme_toggle.ts";
 
 document.addEventListener("DOMContentLoaded", () => {
   initThemeToggle();
+  initMobileMenu();
+
   initContactForm();
 });
---
+++ src/js/mobile_menu.ts
@@ -0,0 +1,23 @@
+export function initMobileMenu(): void {
+  const hamburger = document.querySelector(".sl-c-header__hamburger");
+
+  const mobileMenu = document.querySelector(".sl-c-header__mobile-menu");
+  const closeButton = document.querySelector(".sl-c-header__mobile-menu-close");
+
+  const overlay = document.createElement("div");
+  overlay.classList.add("sl-c-header__overlay");
+  document.body.appendChild(overlay);
+
+  function toggleMenu(): void {
+    mobileMenu?.classList.toggle("is-active");
+    overlay.classList.toggle("is-active");
+
+    document.body.style.overflow = mobileMenu?.classList.contains("is-active")
+      ? "hidden"
+      : "";
+  }
+
+  hamburger?.addEventListener("click", toggleMenu);
+  closeButton?.addEventListener("click", toggleMenu);
+  overlay.addEventListener("click", toggleMenu);
+}

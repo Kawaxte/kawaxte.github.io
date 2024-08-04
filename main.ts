6eead1af45dc278f4923e7f618b7fe7ca7a4ed00
krysyxte <31629699+Kawaxte@users.noreply.github.com>
2024-08-04 10:20:06 +0300

    top-level await allowance

--- main.ts
+++ main.ts
@@ -2,8 +2,4 @@ import { start } from "$fresh/server.ts";
 import config from "./fresh.config.ts";
 import manifest from "./fresh.gen.ts";
 
-async function main(): Promise<void> {
-  await start(manifest, config);
-}
-
-main();
+await start(manifest, config);

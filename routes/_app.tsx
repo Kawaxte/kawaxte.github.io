01558df97999ea91a6af19bbdc50f566c92ca7d8
krysyxte <31629699+Kawaxte@users.noreply.github.com>
2024-08-04 10:20:11 +0300

    cleanup

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,23 +1,28 @@
 import type { PageProps } from "$fresh/server.ts";
 import type { JSX } from "preact";
 
-export default function App({ Component }: PageProps): JSX.Element {
+export default function App(
+  { Component }: PageProps,
+): JSX.Element {
+  const stylesheets = [
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css",
+    "https://fonts.cdnfonts.com/css/corbel",
+    "/styles/base/reset.css",
+    "/styles/base/typography.css",
+    "/styles/components/button.css",
+    "/styles/global.css",
+  ];
+
   return (
     <html>
       <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         <title>Krysyxte</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
-        />
-        <link rel="stylesheet" href="https://fonts.cdnfonts.com/css/corbel" />
-        <link rel="stylesheet" href="/styles/base/reset.css" />
-        <link rel="stylesheet" href="/styles/base/typography.css" />
-        <link rel="stylesheet" href="/styles/components/button.css" />
-        <link rel="stylesheet" href="/styles/global.css" />
+
+        {stylesheets.map((href) => <link rel="stylesheet" href={href} />)}
       </head>
       <body class="bg-gray-100 text-black dark:bg-gray-950 dark:text-gray-50">
         <Component />

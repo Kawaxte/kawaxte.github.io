a6bcb28bdda18eb3291a436e0afb5acd44319fd7
krysyxte <31629699+Kawaxte@users.noreply.github.com>
2024-08-04 11:29:14 +0300

    accent colour adj

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -7,14 +7,14 @@ export default function Error404(): JSX.Element {
     <BaseLayout>
       <ContentLayout>
         <div class="flex flex-col items-center justify-center w-full max-w-md mx-auto">
-          <i class="fas fa-search text-6xl mb-4 text-blue-500"></i>
-          <h1 class="text-4xl font-bold text-blue-500 mb-4">404</h1>
+          <i class="fas fa-search text-6xl mb-4 text-blue-600"></i>
+          <h1 class="text-4xl font-bold text-blue-600 mb-4">404</h1>
           <p class="text-xl text-center mb-6">
             Oops! Cannot find what you're looking for...
           </p>
           <a
             href="/"
-            class="px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline"
+            class="px-4 py-2 font-bold text-white bg-blue-600 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline"
           >
             Go Back
           </a>

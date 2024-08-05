d4fde29f757e891c203dc13466abf0bf72cce239
krysyxte <31629699+krysyxte@users.noreply.github.com>
2024-08-05 19:10:44 +0300

    fix centred textarea

--- components/formInput.tsx
+++ components/formInput.tsx
@@ -11,17 +11,29 @@ interface FormInputProps {
 export default function FormInput(
   { type, name, placeholder, required, rows }: FormInputProps,
 ): JSX.Element {
+  const commonClasses =
+    "w-full px-3 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-600 dark:focus:border-blue-600 focus:outline-none focus:ring";
+
+  if (type === "textarea") {
+    return (
+      <textarea
+        className={`${commonClasses} align-top`}
+        name={name}
+        placeholder={placeholder}
+        required={required}
+        rows={rows || 4}
+        style={{ resize: "vertical", minHeight: "6rem" }}
+      />
+    );
+  }
+
   return (
     <input
-      class="w-full px-3 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-600 dark:focus:border-blue-600 focus:outline-none focus:ring"
+      className={commonClasses}
       type={type}
       name={name}
       placeholder={placeholder}
       required={required}
-      rows={rows}
-      style={type === "textarea"
-        ? { resize: "vertical", minHeight: "6rem" }
-        : {}}
     />
   );
 }
